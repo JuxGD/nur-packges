1e8555df1c0b26c64482a157fdd95471a416765b
Jux <jak@e.email>
2025-09-19 02:16:00 -0300

    lemme just fix some stuff here, incorporate some stuff that was missing

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "Jux's NUR";
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.jux-is-a-nix-maintainer-apparently.url = "git+https://codeberg.org/JuxGD/jux-is-a-nix-maintainer-apparently?ref=main";
   outputs = { self, nixpkgs }:
     let
       forAllSystems = nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed;
