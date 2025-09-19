8b9d94fd45a8ba3c8a985021df095c6d0af98100
JuxGD <jak@e.email>
2025-09-19 05:02:33 -0300

    hmm

--- default.nix
+++ default.nix
@@ -10,7 +10,7 @@
 
 
 let
-  maintainers-list = import builtins.fetchurl "https://codeberg.org/JuxGD/jux-is-a-nix-maintainer-apparently/raw/branch/main/maintainers-list.nix";
+  maintainers-list = builtins.fetchurl "https://codeberg.org/JuxGD/jux-is-a-nix-maintainer-apparently/raw/branch/main/maintainers-list.nix";
 in
 rec {
   noriskclient-launcher-unwrapped = pkgs.callPackage ./pkgs/noriskclient-launcher-unwrapped { inherit maintainers-list; };
